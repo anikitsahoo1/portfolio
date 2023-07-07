3601bd9f47e2df293f1537285a8155737b47cfc1
Anikit Sahoo <126968156+anikitsahoo1@users.noreply.github.com>
2023-07-07 18:21:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ angular.module('portfolioApp', [])
       doc.text("Portfolio", 10, 10);
 
       doc.setFontSize(12);
-      doc.text("Name: " + $scope.portfolio.name, 10, 20);
-      doc.text("Email: " + $scope.portfolio.email, 10, 30);
-      doc.text("Bio: " + $scope.portfolio.bio, 10, 40);
-      doc.text("Skills: " + $scope.portfolio.skills, 10, 50);
+      var splitBio = doc.splitTextToSize($scope.portfolio.bio, 180);
+      var splitSkills = doc.splitTextToSize($scope.portfolio.skills, 180);
+
+      doc.text("Name: " + $scope.portfolio.name, 10, 30);
+      doc.text("Email: " + $scope.portfolio.email, 10, 40);
+      doc.text("Bio:", 10, 50);
+      doc.text(splitBio, 10, 60, { maxWidth: 180, lineHeight: 1.5 });
+      doc.text("Skills:", 10, 100);
+      doc.text(splitSkills, 10, 110, { maxWidth: 180, lineHeight: 1.5 });
 
       doc.save("portfolio.pdf");
     };
   });
+
